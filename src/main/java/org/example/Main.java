084c15d6ff55444b261ac2acefb40c5e69383809
Siddikpatel <patel.siddik369@gmail.com>
2024-10-23 14:32:15 -0300

    dev 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("dsadmjashdjsab");
         System.out.println("dev it");
+        System.out.println("dev it 2");
     }
 }
